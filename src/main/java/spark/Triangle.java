90a5c0d96501edc5a51296f264462e117b8b530d
Kyle Derrick and trevor elvey <trevor.elvey@gmail.com>
2016-02-15 10:48:01 -0800

    pass equilateral and isosceles tests

--- src/main/java/spark/Triangle.java
+++ src/main/java/spark/Triangle.java
@@ -12,4 +12,23 @@ public class Triangle {
   public int getLength1() {
     return mLength1;
   }
+  public int getLength2() {
+    return mLength2;
+  }
+  public int getLength3() {
+    return mLength3;
+  }
+
+  public String checkTriangle() {
+    String result = "";
+    if ((mLength1 + mLength2 <= mLength3) || (mLength1 + mLength3 <= mLength2) || (mLength3 + mLength2 <= mLength1)) {
+      result = "Not a triangle";
+    } else if ((mLength1 == mLength2) && (mLength2 == mLength3)) {
+      result = "Equilateral";
+    } else if (((mLength1 == mLength2) && (mLength2 != mLength3)) || ((mLength1 == mLength3) && (mLength1 != mLength2))) {
+      result = "Isosceles";
+    }
+    return result;
+  }
+
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -11,8 +11,37 @@ public class TriangleTest {
 
   @Test
   public void newTriangle_getLengthOne() {
+    Triangle testTriangle = new Triangle(4,2,2);
+    assertEquals(4, testTriangle.getLength1());
+  }
+
+  @Test
+  public void newTriangle_getLengthThree() {
+    Triangle testTriangle = new Triangle(4,4,2);
+    assertEquals(2, testTriangle.getLength3());
+  }
+
+  @Test
+  public void newTriangle_getLengthTwo() {
     Triangle testTriangle = new Triangle(2,4,4);
-    assertEquals(2, testTriangle.getLength1());
+    assertEquals(4, testTriangle.getLength2());
+  }
+
+  @Test
+  public void newTriangle_checkTriangleType_notATriangle() {
+    Triangle testTriangle = new Triangle(2,2,4);
+    assertEquals("Not a triangle", testTriangle.checkTriangle());
+  }
+
+  @Test
+  public void newTriangle_checkTriangleType_equilateral() {
+    Triangle testTriangle = new Triangle(2,2,2);
+    assertEquals("Equilateral", testTriangle.checkTriangle());
+  }
+  @Test
+  public void newTriangle_checkTriangleType_isosceles() {
+    Triangle testTriangle = new Triangle(3,3,4);
+    assertEquals("Isosceles", testTriangle.checkTriangle());
   }
 
 
